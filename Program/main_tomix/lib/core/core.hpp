732b590300e808d06bd52ace7f757a3329186da5
tomixrm <chocolate0785lego@icloud.com>
2023-10-14 11:31:18 +0900

    :sparkles: ピン配置を追加
    # ==== Emojis ====
    # ✨ :sparkles: Add new feature
    # ✅  :white_check_mark: Update existing feature (not bug)
    # ⚠️  :warning: Fix bug/error
    # 🔥  :fire: Remove files or features
    # ✏️ :pencil2: Fix typos
    # 📝  :memo: Update documentation
    # ♻️  :recycle: Dependencies update
    # 🐧  :penguin: Improved the Linux
    # 🍎  :apple: Improved the macOS
    # 🔖 :bookmark: Add new library
    # 🎉  :tada: Initial commit
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== Messages ====
    # 新規(ファイル)機能追加: Add
    # 機能修正、既存機能追加（バグではない): Update
    # リファクタリング: Clean
    # ファイル名変更:
    # 削除など: Remove
    # バグ修正: Fix
    # バージョンアップ: Upgrade

---
+++ Program/main_tomix/lib/core/core.hpp
@@ -0,0 +1,48 @@
+#ifndef CORE_HPP
+#define CORE_HPP
+
+#include <Arduino.h>
+
+// Core Pins
+namespace CorePins {
+// UART
+const uint8_t TX0 = 0;
+const uint8_t RX0 = 1;
+
+// I2C
+const uint8_t SDA = 4;
+const uint8_t SCL = 5;
+// Encorder
+const uint8_t EncPortA1 = 6;
+const uint8_t EncPortB1 = 7;
+const uint8_t EncPortA2 = 8;
+const uint8_t EncPortB2 = 9;
+
+// Switch
+const uint8_t ENG_SW = 10;
+const uint8_t SW = 11;
+
+// LED
+const uint8_t Debug_LED = 16;
+const uint8_t Alive_LED = 17;
+
+// Servo
+const uint8_t Servo1 = 18;
+const uint8_t Servo2 = 19;
+const uint8_t Servo3 = 22;
+
+// PWM(Motor)
+const uint8_t Motor1 = 20;
+const uint8_t Motor2 = 21;
+
+// ADC
+const uint8_t ADC_0 = 26;
+const uint8_t ADC_1 = 27;
+const uint8_t ADC_2 = 28;
+
+} // namespace CorePins
+
+// PWM Frequency
+const int PWM_FREQ = 100 * 10E3; // 100kHz
+
+#endif
