ac9b1cedb8ab7ae928f3677fe871e5559cc8ea3f
tomixrm <chocolate0785lego@icloud.com>
2023-10-16 18:10:11 +0900

    :sparkles: change encorder lib to Outer
    # ==== Emojis ====
    # ✨ :sparkles: Add new feature
    # ✅  :white_check_mark: Update existing feature (not bug)
    # ⚠️  :warning: Fix bug/error
    # 🔥  :fire: Remove files or features
    # ✏️ :pencil2: Fix typos
    # 📝  :memo: Update documentation
    # ♻️  :recycle: Dependencies update
    # 🐧  :penguin: Improved the Linux
    # 🍎  :apple: Improved the macOS
    # 🔖 :bookmark: Add new library
    # 🎉  :tada: Initial commit
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== Messages ====
    # 新規(ファイル)機能追加: Add
    # 機能修正、既存機能追加（バグではない): Update
    # リファクタリング: Clean
    # ファイル名変更:
    # 削除など: Remove
    # バグ修正: Fix
    # バージョンアップ: Upgrade

--- Program/main_tomix/src/main.cpp
+++ Program/main_tomix/src/main.cpp
@@ -1,13 +1,20 @@
 #include "Robot.hpp"
-
+#include "TickTwo.h"
 PioEncoder encA1(CorePins::EncPortA1);
 Motor motor1(CorePins::Motor1, &encA1);
 
+void printMessage() {
+    Serial.println("Hello!");
+}
+
+TickTwo tick1(printMessage, 0, 1);
+
 void setup() {
-    motor1.begin();
-    motor1.setGain(1, 1, 0);
-    motor1.setLimit(120, -120);
+    // motor1.begin();
+    // motor1.setGain(1, 1, 0);
+    // motor1.setLimit(120, -120);
+    tick1.start();
 }
 void loop() {
-    motor1.drive(100);
+    tick1.update();
 }
