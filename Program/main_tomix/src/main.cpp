0029a72586e0af2fad17286d0023857b78e2b887
tomixrm <chocolate0785lego@icloud.com>
2023-10-23 00:39:43 +0900

    :sparkles: TickTwoを使った処理を書いた
    # ==== Emojis ====
    # ✨ :sparkles: Add new feature
    # ✅  :white_check_mark: Update existing feature (not bug)
    # ⚠️  :warning: Fix bug/error
    # 🔥  :fire: Remove files or features
    # ✏️ :pencil2: Fix typos
    # 📝  :memo: Update documentation
    # ♻️  :recycle: Dependencies update
    # 🐧  :penguin: Improved the Linux
    # 🍎  :apple: Improved the macOS
    # 🔖 :bookmark: Add new library
    # 🎉  :tada: Initial commit
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== Messages ====
    # 新規(ファイル)機能追加: Add
    # 機能修正、既存機能追加（バグではない): Update
    # リファクタリング: Clean
    # ファイル名変更:
    # 削除など: Remove
    # バグ修正: Fix
    # バージョンアップ: Upgrade

--- Program/main_tomix/src/main.cpp
+++ Program/main_tomix/src/main.cpp
@@ -3,18 +3,27 @@
 PioEncoder encA1(CorePins::EncPortA1);
 Motor motor1(CorePins::Motor1, &encA1);
 
-void printMessage() {
+TickTwo tick1(
+    [](){
     Serial.println("Hello!");
-}
-
-TickTwo tick1(printMessage, 0, 1);
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));}
+    , 500);
 
 void setup() {
-    // motor1.begin();
-    // motor1.setGain(1, 1, 0);
-    // motor1.setLimit(120, -120);
+    Serial.begin(9600);
+    pinMode(LED_BUILTIN, OUTPUT);
+    delay(100);
+
+
     tick1.start();
 }
+
 void loop() {
+    delay(1000);
+}
+
+// タイマー系タスクはloop1に書いて，loopでは時間がかかるタスクを処理する．
+// デュアルコアを用いる．
+void loop1() {
     tick1.update();
 }
