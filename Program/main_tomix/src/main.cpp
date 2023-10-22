fbf10cea0d43d8879fe7d8adf8d8771e4c12379f
tomixrm <chocolate0785lego@icloud.com>
2023-10-23 00:51:08 +0900

    :sparkles: PICOのEEPROM(FLASH)の書き込みと読み出しのプログラムの検証成功
    # ==== Emojis ====
    # ✨ :sparkles: Add new feature
    # ✅  :white_check_mark: Update existing feature (not bug)
    # ⚠️  :warning: Fix bug/error
    # 🔥  :fire: Remove files or features
    # ✏️ :pencil2: Fix typos
    # 📝  :memo: Update documentation
    # ♻️  :recycle: Dependencies update
    # 🐧  :penguin: Improved the Linux
    # 🍎  :apple: Improved the macOS
    # 🔖 :bookmark: Add new library
    # 🎉  :tada: Initial commit
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== Messages ====
    # 新規(ファイル)機能追加: Add
    # 機能修正、既存機能追加（バグではない): Update
    # リファクタリング: Clean
    # ファイル名変更:
    # 削除など: Remove
    # バグ修正: Fix
    # バージョンアップ: Upgrade

--- Program/main_tomix/src/main.cpp
+++ Program/main_tomix/src/main.cpp
@@ -1,29 +1,79 @@
 #include "Robot.hpp"
-#include "TickTwo.h"
-PioEncoder encA1(CorePins::EncPortA1);
-Motor motor1(CorePins::Motor1, &encA1);
 
-TickTwo tick1(
-    [](){
-    Serial.println("Hello!");
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));}
-    , 500);
+// 以下のコメントアウトを外すと、EEPROMの読み書きを行う
+#define EEPROM_READ
+// #define EEPROM_WRITE
+
+#ifdef EEPROM_READ
+#include <EEPROM.h>
+
+// start reading from the first byte (address 0) of the EEPROM
+int address = 0;
+byte value;
 
 void setup() {
-    Serial.begin(9600);
-    pinMode(LED_BUILTIN, OUTPUT);
-    delay(100);
-
-
-    tick1.start();
+    // initialize serial and wait for port to open:
+    Serial.begin(115200);
+    EEPROM.begin(512);
 }
 
 void loop() {
-    delay(1000);
+    // read a byte from the current address of the EEPROM
+    value = EEPROM.read(address);
+
+    Serial.print(address);
+    Serial.print("\t");
+    Serial.print(value, DEC);
+    Serial.println();
+
+    // advance to the next address of the EEPROM
+    address = address + 1;
+
+    // there are only 512 bytes of EEPROM, from 0 to 511, so if we're
+    // on address 512, wrap around to address 0
+    if (address == 512) {
+        address = 0;
+    }
+
+    delay(10);
 }
 
-// タイマー系タスクはloop1に書いて，loopでは時間がかかるタスクを処理する．
-// デュアルコアを用いる．
-void loop1() {
-    tick1.update();
+#endif
+
+#ifdef EEPROM_WRITE
+/*
+   EEPROM Write
+
+   Stores values read from analog input 0 into the EEPROM.
+   These values will stay in the EEPROM when the board is
+   turned off and may be retrieved later by another sketch.
+*/
+
+#include <EEPROM.h>
+
+// the current address in the EEPROM (i.e. which byte
+// we're going to write to next)
+int addr = 0;
+
+void setup() {
+    Serial.begin(115200);
+    EEPROM.begin(512);
 }
+
+void loop() {
+    for (uint8_t i = 0; i < 255; i++) {
+        EEPROM.write(addr, 255 - i);
+        addr = addr + 1;
+        if (addr == 255) {
+            addr = 0;
+            if (EEPROM.commit()) {
+                Serial.println("EEPROM successfully committed");
+            } else {
+                Serial.println("ERROR! EEPROM commit failed");
+            }
+        }
+        delay(50);
+    }
+}
+
+#endif
