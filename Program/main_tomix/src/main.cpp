7465a87ac3f3015acef2e21d14b73e2f773e21c1
tomixrm <chocolate0785lego@icloud.com>
2023-11-13 00:59:20 +0900

    PIDライブラリを修正した

--- Program/main_tomix/src/main.cpp
+++ Program/main_tomix/src/main.cpp
@@ -1,70 +1,33 @@
-#include "Arduino.h"
+#include <Robot.hpp>
+#include <./Modes/MainMode.hpp>
+#include <./Modes/ModeTemp.hpp>
+#include <./Modes/SystemIdentification.hpp>
 
-#include "RPi_Pico_TimerInterrupt.h"
+#define MODE_QTY 3
 
-// タイマー割り込みのオブジェクトを生成する
-RPI_PICO_Timer ITimer0(0);
-RPI_PICO_Timer ITimer1(1);
-RPI_PICO_Timer ITimer3(3);
+MainMode mainMode('M', "MainMode");
+TempMode tempMode('T', "TempMode");
+SystemIdentificationMode systemidentificationMode('S', "SI Mode");
 
-// ピン番号を定義する
-#define LED_DEBUG 16
-#define LED_BUILTIN 25
-#define LED_ALIVE 17
-
-// タイマー割り込みハンドラを定義する(今回の中身は Lチカ関数)
-bool TimerHandler0(struct repeating_timer *t) {
-    (void)t;
-    digitalWrite(LED_DEBUG, digitalRead(LED_DEBUG) ^ 1);
-    return true;
-}
-
-bool TimerHandler1(struct repeating_timer *t) {
-    (void)t;
-    digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) ^ 1);
-    return true;
-}
-
-bool TimerHandler2(struct repeating_timer *t) {
-    (void)t;
-    digitalWrite(LED_ALIVE, digitalRead(LED_ALIVE) ^ 1);
-    return true;
-}
+Mode *modes[MODE_QTY] = {&mainMode, &tempMode, &systemidentificationMode};
+Mode *currentMode = &tempMode;
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(LED_DEBUG, OUTPUT);
-    pinMode(LED_ALIVE, OUTPUT);
-
-    Serial.begin(115200);
-
-    // 設定とかをプリントする
-    Serial.println(BOARD_NAME);
-    Serial.println(RPI_PICO_TIMER_INTERRUPT_VERSION);
-    Serial.print(F("CPU Frequency = "));
-    Serial.print(F_CPU / 1000000);
-    Serial.println(F(" MHz"));
-
-    // タイマー割り込みハンドラの設定 0.5s
-    if (ITimer0.attachInterruptInterval(0.5 * 1000 * 1000, TimerHandler0)) {
-        Serial.print(F("attach!!"));
-    } else
-        Serial.println(F("Can't set ITimer0. Select another freq. or timer")); // エラー処理
-
-    //  タイマー割り込みハンドラの設定 1s
-    if (ITimer1.attachInterruptInterval(1 * 1000 * 1000, TimerHandler1)) {
-        Serial.print(F("attach!!"));
-    } else
-        Serial.println(F("Can't set ITimer1. Select another freq. or timer")); // エラー処理
-
-    // タイマー割り込みハンドラの設定 0.25s
-    if (ITimer3.attachInterruptInterval(0.125 * 1000 * 1000, TimerHandler2)) {
-        Serial.print(F("attach!!"));
-    } else
-        Serial.println(F("Can't set ITimer3. Select another freq. or timer")); // エラー処理
+    mainMode.init();
+    tempMode.init();
+    systemidentificationMode.init();
 }
 
 void loop() {
-    delay(1000);
-    Serial.println("loop");
+    if (Serial.available() > 0) {
+        char c = Serial.read();
+        for (unsigned i = 0; i < MODE_QTY; ++i) {
+            if (c == modes[i]->getModeLetter() && currentMode != modes[i]) {
+                currentMode->after();
+                currentMode->before();
+                currentMode = modes[i];
+            }
+        }
+    }
+    currentMode->loop();
 }
