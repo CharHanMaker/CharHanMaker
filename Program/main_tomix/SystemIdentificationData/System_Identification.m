40285d7bec5f4296ea8845a1670313606836130a
ryoskRFR <wvcohry@gmail.com>
2023-11-14 13:50:48 +0900

    matlabのsystem identificationで伝達関数増やした

--- Program/main_tomix/SystemIdentificationData/System_Identification.m
+++ Program/main_tomix/SystemIdentificationData/System_Identification.m
@@ -22,9 +22,18 @@
 
 % G3_0  71.35%
 
+% G3_1  78.25%
+%            -1.97e07 s - 1.829e07
+%   ---------------------------------------
+%   s^3 + 219.2 s^2 + 1.343e07 s + 7.726e06
+
+% G3_2  71.36%
+%     -17.4 s^2 + 2.96 s - 21.95
+%   ------------------------------
+%   s^3 + 10 s^2 + 1.216 s + 12.16
+
 clear;
 clc;
-% file = "./Code/CharHanMaker/Program/main_tomix/SystemIdentificationData/SIdata_1_average4_2023-11-07.csv";
 file = "SIdata_1_average4_2023-11-07_controlable.csv";
 
 FR.u = readmatrix(file, "Range", [2 2 6297 2]); % Volt
@@ -42,10 +51,12 @@ FRdata_det = detrend(FRdata);%データの平均値を0にする
 G1_0 = tfest(FRdata_det, 1, 0);%極が1、零が0のモデルを同定
 G1_1 = tfest(FRdata_det, 1, 1);%極が1、零が1のモデルを同定
 % G2_0 = tfest(FRdata_det, 2, 0);%極が2、零が0のモデルを同定
-% G3_0 = tfest(FRdata_det, 3, 0);%極が3、零が0のモデルを同定
 % G2_1 = tfest(FRdata_det, 2, 1);%極が2、零が1のモデルを同定
+% G3_0 = tfest(FRdata_det, 3, 0);%極が3、零が0のモデルを同定
+G3_1 = tfest(FRdata_det, 3, 1)%極が3、零が0のモデルを同定
+G3_2 = tfest(FRdata_det, 3, 2)%極が3、零が0のモデルを同定
 
-figure(2), compare(FRdata,G1_0, G1_1);%モデルの応答比較の表示
+figure(2), compare(FRdata,G1_0, G1_1, G3_1, G3_2);%モデルの応答比較の表示
 
 % figure(3), bode(G1_0);
 % figure(4), bode(G1_1);
