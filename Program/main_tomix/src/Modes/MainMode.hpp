fe7f687a1be7a288b5a0048b1c5e466108d7f60f
tomixrm <chocolate0785lego@icloud.com>
2023-11-30 00:18:47 +0900

    :sparkles:  新チャーハン基板でエンコーダが読めたことが確認できた
    # 注意
    
    「以下のように2つ以上のエンコーダを読もうとしないとエンコーダを読むことができない仕様があります．」と思っていたら，何故か単体でも動いたわなんでやw
    
    ↓↓コレネ↓↓
    float angle0 = AbsEncorders.readDegree(0);
    float angle1 = AbsEncorders.readDegree(1);
    float vel0 = AbsEncorders.getVelocity(0);
    float vel1 = AbsEncorders.getVelocity(0);
    
    なんでや

--- Program/main_tomix/src/Modes/MainMode.hpp
+++ Program/main_tomix/src/Modes/MainMode.hpp
@@ -18,17 +18,19 @@ class MainMode : public Mode, Robot {
     }
 
     void loop() {
-        // モータを回すプログラムです．
-        int run = 65535 / 2 * MyMath::sinDeg(i);
-        motorA.runOpenloop(run, true); // これで無理やりモータを回せます．
-        // mototA.drive(velocity);// :TODO:@ryoskRFR ←←ここにPID制御の速度制御を入れたいよね
-        delay(50);
-        Serial.println(run);
-        i++;
-    }
+        float angle0 = AbsEncorders.readDegree(0);
+        float angle1 = AbsEncorders.readDegree(1);
+        float vel0 = AbsEncorders.getVelocity(0);
+        float vel1 = AbsEncorders.getVelocity(0);
+        Serial.printf("%f, %f, %f, %f\n", angle0, angle1, vel0, vel1);
 
-    void after() {
-        motorA.stop();
+        // // モータを回すプログラムです．
+        // int run = 65535 / 2 * MyMath::sinDeg(i);
+        // motorA.runOpenloop(run, true); // これで無理やりモータを回せます．
+        // // mototA.drive(velocity);// :TODO:@ryoskRFR ←←ここにPID制御の速度制御を入れたいよね
+        // delay(50);
+        // Serial.println(run);
+        // i++;
     }
 
   private:
