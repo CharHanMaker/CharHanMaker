c2f4c618a28ddb38374af6e0deed35293fe35786
ryoskRFR <wvcohry@gmail.com>
2024-01-02 15:28:37 +0900

    凡例とか修正した

--- Program/main_tomix/SystemIdentificationData/PlotSimulations.m
+++ Program/main_tomix/SystemIdentificationData/PlotSimulations.m
@@ -4,7 +4,7 @@ file1 = 'Synchro_nishi.slx';
 file2 = 'Synchro_tommy.slx';
 file3 = 'TDoFCS.slx';
 
-sim(file3);
+sim(file1);
 
 targetAngleVel = synchro_vel.Data(:,1);
 vel0 = synchro_vel.Data(:,2);
@@ -17,25 +17,25 @@ angleError = synchro_rad.Data(:,3);
 figure(1)
 plot(tout, targetAngleVel);
 hold on
-plot(tout, vel0);
 plot(tout, vel1);
+plot(tout, vel0);
 ylim([-2, 22]);
 xlabel('時間 [s]');
 ylabel('角速度 [rad/s]');
-legend("目標角速度", "モータ0の角速度", "モータ1の角速度");
+legend("目標角速度", "モータAの角速度", "モータBの角速度");
 title('目標角速度とモータの角速度');
 grid on
 hold off
 
 figure(2)
-plot(tout, rad0);
-hold on
 plot(tout, rad1);
+hold on
+plot(tout, rad0);
 plot(tout, angleError);
 ylim([-20, 180]);
 xlabel('時間 [s]');
 ylabel('角 [rad]');
-legend("モータ0の角", "モータ1の角", "2モータの同期偏差");
+legend("モータAの角", "モータBの角", "2モータの同期偏差");
 title('2モータの同期');
 grid on
 zp = BaseZoom();
